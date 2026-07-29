927d81c30a932d42d9023a24d7dd97d1de3e66c4
Indra Chaganti <146953091+INDRA723@users.noreply.github.com>
2026-07-29 11:02:27 +0530

    Refactor DBConnection to use environment variables

--- TransactionManagementSystem/src/com/tms/db/DBConnection.java
+++ TransactionManagementSystem/src/com/tms/db/DBConnection.java
@@ -6,9 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/transaction_db";
-    private static final String USERNAME = "root";         // your MySQL username
-    private static final String PASSWORD = "enter your password"; // your MySQL password
+    private static final String HOST = System.getenv("MYSQLHOST");
+    private static final String PORT = System.getenv("MYSQLPORT");
+    private static final String DB = System.getenv("MYSQLDATABASE");
+    private static final String USERNAME = System.getenv("MYSQLUSER");
+    private static final String PASSWORD = System.getenv("MYSQLPASSWORD");
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB + "?useUnicode=true&sslMode=REQUIRED";
 
     private static Connection connection = null;
 
@@ -17,17 +20,18 @@ public class DBConnection {
             if (connection == null || connection.isClosed()) {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-                System.out.println("✔ Database connected!");
+                System.out.println("✓ Database connected!");
             }
         } catch (ClassNotFoundException e) {
-            System.out.println("✘ MySQL Driver not found!");
+            System.out.println("✗ MySQL Driver not found!");
             e.printStackTrace();
         } catch (SQLException e) {
-            System.out.println("✘ Connection failed! Check credentials.");
+            System.out.println("✗ Connection failed! Check credentials.");
             e.printStackTrace();
         }
         return connection;
     }
+
 
     public static void closeConnection() {
         try {
